f45c792eef5dc132264fcee8f01c61723af86687
hardiksingh98 <hardik.singh@turtlemint.com>
2020-08-23 22:41:54 +0530

    added styline and htm

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ProjectItem from "./Projects/ProjectItem";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 class DashBoard extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Welcome to Dashboard</h1>
-        <ProjectItem />
-        <ProjectItem />
-        <ProjectItem />
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                Create a Project
+              </a>
+              <br />
+              <hr />
+
+              <ProjectItem />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
